aea9578d8851318f4bc7cba4f4c0717e8884e974
diegoss <d90554686@gmail.com>
2023-07-05 18:13:06 -0500

    ASTA CATEGO

--- funciones/funcionescategoria.php
+++ funciones/funcionescategoria.php
@@ -21,4 +21,27 @@ function getAllCategorias()
   }
 }
 
+// RETORNA UN REGISTRO
+function getNombreCategoriaById($catego_id)
+{
+  try {
+    $sql = "SELECT catego_descripcion FROM tab_categorias
+          WHERE catego_id=:pcatego_id";
+    $conexion = conectaBaseDatos();
+    $stmt = $conexion->prepare($sql);
+    $stmt->bindparam(":pcatego_id", $marca_id);
+    $stmt->execute();
+    if ($stmt->rowCount() > 0) {
+      $registro = $stmt->fetch(PDO::FETCH_ASSOC);
+      return $registro['catego_descripcion'];
+    } else {
+      return null;
+    }
+  } catch (PDOException $e) {
+    echo $e->getMessage();
+    return null;
+  }
+}
+
+
 ?>
